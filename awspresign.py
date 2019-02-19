a02dd2cf69aa734633998274119093a6d7866438
Niranjan Sathindran <niranjan.sathindran@csra.com>
2019-02-19 10:46:11 -0500

    First Commit

---
+++ awspresign.py
@@ -0,0 +1,12 @@
+import boto3
+
+bucket="<sample-bucket>"
+key="<object-name-from-bucket>"
+expiration=1000 * 60 * 60 * 72 #Modify 72 here to enter number of hours
+params = {
+    'Bucket': bucket,
+    'Key': key
+}
+s3 = boto3.client('s3')
+url = s3.generate_presigned_url('get_object', Params=params, ExpiresIn=expiration)
+print('url:' + url)
